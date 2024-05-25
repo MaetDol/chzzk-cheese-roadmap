d5ada1fa6f56293101971e6e8b63342a65d5abee
MaetDol <rndka9@gmail.com>
2024-05-26 00:57:16 +0900

    아이콘 컴포넌트 생성

---
+++ src/components/Icon/index.ts
@@ -0,0 +1,9 @@
+import { ChatDonation } from '@/components/Icon/ChatDonation';
+import { MissionDonation } from '@/components/Icon/MissionDonation';
+import { VideoDonation } from '@/components/Icon/VideoDonation';
+
+export const Icon = {
+  ChatDonation,
+  MissionDonation,
+  VideoDonation,
+};
