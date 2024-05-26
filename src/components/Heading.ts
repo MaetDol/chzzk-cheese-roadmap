9408e5ea17f89668b6e89ddb06252c5beecb0879
MaetDol <rndka9@gmail.com>
2024-05-26 14:44:09 +0900

    feat, Text 에 size 속성 추가

--- src/components/Heading.ts
+++ src/components/Heading.ts
@@ -2,6 +2,7 @@ import { Text } from '@/components/Text';
 
 export function Heading(level: 1 | 2 | 3 | 4, text: string) {
   const heading = document.createElement(`h${level}`);
-  heading.append(Text(text, true));
+  heading.append(Text(text, { useChzzkFont: true, size: 'initial' }));
+
   return heading;
 }
